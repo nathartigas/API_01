389e1c02b2f4ac3758811de9a6cc928044d2736a
nathartigas <nathartigasna@gmail.com>
2024-04-17 15:57:33 -0300

    first commit

--- api.js
+++ api.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 //IMPORTS
 const mysql_config = require('./imp/functions')
 const functions = require('./imp/functions')
+const { Connection } = require('mysql2/typings/mysql/lib/Connection')
 
 //variaveis para disponibilidade e para versionamento
 const API_AVAILABILITY = true;
@@ -48,6 +49,41 @@ app.get('./tasks',(req,res)=>{
         }
     })
 })
+
+//rota para fazer uma consulta de task por id
+app.get('/tasks/id',(req,res)=>{
+    const id =req.params.id
+    connection.query('SELECT * FROM tasks WHERE id = ?',[id],(err,rows)=>{
+        if(err){
+            if(rows.length>0){
+            res.json(functions.response('sucesso','sucesso na pesquisa',rows.length,rows))
+            }else{
+                res.json(functions.response('atnção','nao foi encontrada a task selecionada',0,null))
+            }
+        }else{
+            res.json(functions.response('erro',err.message,0,null))
+        }
+    })
+})
+
+//rota para atualizar o status da  task pelo id selecionado
+app.put('/tasks/:id/status/:status',(req,res)=>{
+    const id = req.params.id;
+    const status = req.params.status;
+    connection.query('UPDATE tasks SET status =? WHERE id=?',[status,id],(err,rows)=>{
+        if(!err){
+            if(rows.affectedRows>0){
+                res.json(functions.response ('sucesso','sucesso na alteração do status',rows.affectedRows,null))
+            }else{
+                res.json(functions.response ('alerta vermelho','task nao',rows.affectedRows,0,null))
+            }
+        }else{
+            res.json(functions.response ('erro',err.message,0,null))
+        }
+    })
+})
+
+
 
 //tratar o erroo da rota
 app.use((req,res)=>{
